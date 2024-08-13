a2a73dd2028e81ec41e6c8ec4be2ed4551dc7f9c
RavinduRR <rshan9740@gmail.com>
2024-08-14 00:30:54 +0530

    1

---
+++ lib/main-page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea (
+        child: Container(
+          color: Colors.white,
+          child: const Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.male,size: 150, ),
+                        Text("Male"),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.female, size: 150,),
+                        Text("Female"),
+                      ],
+                    ),
+                  )
+                ],
+                
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text("Hight"),
+                        Text("176",
+                        style: TextStyle(
+                        color: Color.fromARGB(255, 240, 87, 87),
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold
+                        ),),
+                        
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.female, size: 150,),
+                        Text("Female"),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
